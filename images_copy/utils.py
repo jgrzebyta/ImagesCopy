6f44de00c63e698f3b55303fe61713be328ad803
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2019-10-04 00:33:52 +0100

    Add copy_file

--- images_copy/utils.py
+++ images_copy/utils.py
@@ -1,6 +1,9 @@
 import logging
 import os
+import collections
+import shutil
 from os import walk
+from os.path import sep
 
 
 class FilePath(object):
@@ -17,6 +20,10 @@ class FilePath(object):
     def extension(self):
         return self.filename.split('.')[-1].lower()
 
+    def file_prefix(self):
+        lastDot = self.filename.rindex('.')
+        return self.filename[0:lastDot]
+
 
 def list_files(directory):
     """List all files inside a [[directory]]"""
@@ -27,3 +34,20 @@ def list_files(directory):
         for fs in filenames:
             out_array.append(FilePath(dirpath, fs))
     return out_array
+
+
+def copy_file(original, target_directory):
+    logger = logging.getLogger('root')
+    assert(isinstance(original, FilePath))
+    assert(os.path.exists(target_directory))
+    splited_dirs = collections.deque(original.directory.split(sep))
+    splited_dirs.popleft()
+    copy_directory = sep.join([target_directory, sep.join(splited_dirs)])
+    target_file = FilePath(copy_directory, original.filename)
+    try:
+        os.makedirs(target_file.directory)
+        shutil.copy2(original.as_path(), target_file.as_path())
+        logger.info('%s -> %s', original.as_path(), target_file.as_path())
+    except FileExistsError as err:
+        logger.warning('File exists: %s', err.filename)
+    return target_file
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from collections.abc import Iterable
 from images_copy import utils, log
 from images_copy.utils import FilePath
@@ -25,3 +26,18 @@ class TestUtils(unittest.TestCase):
     def test_TXT_extension(self):
         mockTextFile = FilePath('/tmp/mock_fdata', 'some_file.TXT')
         self.assertEqual('txt', mockTextFile.extension())
+
+    def test_filename_without_ext(self):
+        mockTextFile = FilePath('/tmp/mock_fdata', 'some_file.txt')
+        self.assertEqual('some_file', mockTextFile.file_prefix())
+
+    def test_long_filename_without_ext(self):
+        mockTextFile = FilePath('/tmp/mock_fdata', 'some_file.tar.gz')
+        self.assertEqual('some_file.tar', mockTextFile.file_prefix())
+
+    def test_copy_file(self):
+        original = FilePath('./images_copy', 'utils.py')
+        target_directory = '/tmp'
+        actual = utils.copy_file(original, target_directory)
+        self.assertTrue(isinstance(actual, FilePath))
+        self.assertTrue(os.path.exists(actual.as_path()))
